c9fabb7ec82ff0d599f87d8b66384ee5bbf4b0a8
EricMint <shaozhiqi@gmail.com>
2017-10-23 20:30:09 +0800

    remove request mapping from controller

--- src/main/java/com/hospital/bysy/controller/ReportPatientController.java
+++ src/main/java/com/hospital/bysy/controller/ReportPatientController.java
@@ -5,22 +5,16 @@ import com.hospital.bysy.apiservice.ReportPatientAPIService;
 import com.hospital.bysy.service.ReportPatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by mint on 10/18/17.
  */
 @Controller
-public class ReportPatientController {
+public class ReportPatientController implements ReportPatientAPIService {
 
     @Autowired
     ReportPatientService reportPatientService;
 
-    @RequestMapping(value = "/imaging-examination/category-list", method = RequestMethod.GET)
-    @ResponseBody
     public ImagingExaminationCategoryInfoListResponse getImagingExaminationCategoryList() {
         return reportPatientService.getImagingExaminationCategoryList();
     }
